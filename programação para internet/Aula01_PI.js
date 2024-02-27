0ac581e344522d0a2b075d473616d8bb897461ff
João Deybson <99667175+20201174010029@users.noreply.github.com>
2024-02-27 16:14:40 -0300

    .

---
+++ programação para internet/Aula01_PI.js
@@ -0,0 +1,57 @@
+nome = "Joao Deybson";
+idade = 28;
+
+const altura = 1.62;
+
+let bol = true
+let lista = ["banana", "maça", "uva"];
+let objeto = {nome: "maria", idade: "18"};
+
+// Operações :
+
+let a = 10;
+let b = 5;
+
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao =  a * b;
+let divisao = a / b;
+let resto = a % b;
+
+// operadores de atribuição:
+
+let x = 10;
+
+x+= 5; // x = x + 5 (15)
+x-= 3;
+x*= 2;
+x/= 4;
+
+// operadores de comparação:
+
+idade = 20;
+let temCNH = true;
+
+console.log(idade > 18); // idade é maior que 18 ??? (TRUE)
+console.log(idade === 20);//
+console.log(idade !== 18);//
+
+/* 
+igualdade solta
+5 == '5' ()
+
+igualdade estrita
+5 = '5' ()
+
+0 == false (True)
+0 === false (False)
+*/
+
+// comparadore logicos
+
+console.log(idade > 18 && temCNH); // a idade é maior que 18 e possui cnh?
+// console.log(idade > 18 ll temCNH);
+
+let hora = 12;
+
+// lanços de repetição( for while), condições e contadores. 
